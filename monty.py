c6a195d326110632f716d371ccb283c1c3e7d982
areon546 <57325743+areon546@users.noreply.github.com>
2024-10-30 12:41:13 +0000

    Broke up the main method into submethods

--- monty.py
+++ monty.py
@@ -6,19 +6,19 @@ import math
 def generateCoordinate(RADIAL : bool, xs, ys) -> int:
     
     if (RADIAL):
-        xPos, yPos, radius = generateCoordinatesRadially(xs, ys)
+        xPos, yPos, radius = generateCoordinateRadially(xs, ys)
     else:
-        xPos, yPos = generateCoordinatesUniformly(xs, ys)
+        xPos, yPos = generateCoordinateUniformly(xs, ys)
         radius = (xPos**2 + yPos**2)**(1/2)
     
-    xCoords.append(xPos)
-    yCoords.append(yPos)
+    xs.append(xPos)
+    ys.append(yPos)
     
     # print(radius)
     
     return radius
 
-def generateCoordinatesRadially(xs, ys):
+def generateCoordinateRadially(xs, ys):
     
     # generate a random angle
     theta = rng.uniform(0,2*math.pi)
@@ -33,37 +33,23 @@ def generateCoordinatesRadially(xs, ys):
     
     return xPos, yPos, radius
 
-def generateCoordinatesUniformly(xs, ys):
+def generateCoordinateUniformly(xs, ys):
     xPos = rng.uniform(-1.0, 1.0)
     yPos = rng.uniform(-1.0, 1.0)
     
     return xPos, yPos
 
-
-
-if __name__ == "__main__":
-    # print("Hello, World!")
-
-    # here i have the arrays and other important things
-    xCoords = []
-    yCoords = []
-    metricArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    imperialArr = [0, 0, 0, 0, 0]
-    metricArrR = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    imperialArrR = [0, 0, 0, 0, 0]
-    loops = 5_00 #0
-    inC = 0
+def generateCoordinates(IS_RADIAL, loops, numberOfShots, metricArr, imperialArr):
+    xs = []
+    ys = []
     counter = 0
     maxR = 1
-    IS_RADIAL = False
-
-
-    # generates points and places them in the arrays
+    
     while counter<loops:
         counted = False
 
         # here i make the random numbers
-        radius = generateCoordinate(IS_RADIAL, xCoords, yCoords)      
+        radius = generateCoordinate(IS_RADIAL, xs, ys)      
         #print(f"x: {xPos}, y: {yPos}, r: {radius}")
 
 
@@ -77,11 +63,58 @@ if __name__ == "__main__":
                 imperialArr[i//2] += 1
                 counted=True
                 #print("metric counted")
-                inC+=1
+                numberOfShots+=1
         
         counter+=1
+    
+    return metricArr, imperialArr, xs, ys, numberOfShots
+
+def createUniformImage():
+    
+    
+    
+    return
+
+def makeFigure(ax):
+    # Makes the rings and places them on a figure
+    circles = []
+    
+    # outermost ring boundary
+    circles.append(plt.Circle((0, 0), (1), 
+                            color='black', fill=False)) # 1  outer white ring boundary
+    
+    # colour boundaries
+    circles.append(plt.Circle((0, 0), (0.8), 
+                            color='black', fill=True)) # .8  outer black ring boundary
+    circles.append(plt.Circle((0, 0), (0.6), 
+                            color='blue', fill=True)) # .6  outer blue ring boundary
+    circles.append(plt.Circle((0, 0), (0.4), 
+                            color='red', fill=True)) # .4  outer red ring boundary
+    circles.append(plt.Circle((0, 0), (0.2), 
+                            color='gold', fill=True)) # .2  outer gold ring boundary
+    
+    # outer inner ring boundaryes, only used for metric
+    circles.append(plt.Circle((0, 0), (0.9), 
+                            color='black', fill=False)) # .9  inner white ring boundary
+    circles.append(plt.Circle((0, 0), (0.7), 
+                            color='white', fill=False)) # .7
+    circles.append(plt.Circle((0, 0), (0.5), 
+                            color='black', fill=False)) # .5
+    circles.append(plt.Circle((0, 0), (0.3), 
+                            color='black', fill=False)) # .3
+    circles.append(plt.Circle((0, 0), (0.1), 
+                            color='black', fill=False)) # .1
 
 
+    for i in range(0, 10):
+        ax.add_patch(circles[i])
+    return
+
+def calculateAndPrintResults(numberOfShots, metricArr, imperialArr, loops):
+    metricArrR = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    imperialArrR = [0, 0, 0, 0, 0]
+    
+    
 
     for i in range(0, 10):
         metricArrR[i] = metricArr[i]*(10-i)
@@ -92,15 +125,15 @@ if __name__ == "__main__":
         #print("imperial", i, 9-2*i)
         
 
-    # prints out results
+    
     print("sum divided by number in circle")
     print("\nMetric")
     print(metricArr, metricArrR, ", sum: ", sum(metricArrR),
-        ", avg: ", (sum(metricArrR)/inC))
+        ", avg: ", (sum(metricArrR)/numberOfShots))
 
     print("\nImperial")
     print(imperialArr, imperialArrR, ", sum: ", sum(imperialArrR), 
-        ", avg: ", (sum(imperialArrR)/inC))
+        ", avg: ", (sum(imperialArrR)/numberOfShots))
 
     print("\n")
 
@@ -109,47 +142,39 @@ if __name__ == "__main__":
         ", avg: ", (sum(metricArrR)/loops))
     print(imperialArr, imperialArrR, ", sum: ", sum(imperialArrR), 
         ", avg: ", (sum(imperialArrR)/loops))
+    
+    return
+
+def displayShots(loops):
+    # print("Hello, World!")
+    loops = 5_00 #0
+    IS_RADIAL = False
+
+    # here i have the arrays and other important things
+    metricArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    imperialArr = [0, 0, 0, 0, 0]
+    numberOfShots = 0
+
+
+    # generates points and places them in the arrays
+    metricArr, imperialArr, xs, ys, numberOfShots = generateCoordinates(IS_RADIAL, loops, numberOfShots, metricArr, imperialArr)
+
+
+    # calculates and prints out results
+    calculateAndPrintResults(numberOfShots, metricArr, imperialArr, loops)
 
     fig, ax = plt.subplots()
 
     # sets the limits of the axes
     ax.set_xlim((-1, 1))
     ax.set_ylim((-1, 1))
-
-    #Makes the circles
-    circles = []
-
-    circles.append(plt.Circle((0, 0), (1), 
-                            color='black', fill=False)) # 1
-    circles.append(plt.Circle((0, 0), (0.9), 
-                            color='black', fill=False)) # .9
-    circles.append(plt.Circle((0, 0), (0.8), 
-                            color='black', fill=True)) # .8
-    circles.append(plt.Circle((0, 0), (0.7), 
-                            color='white', fill=False)) # .7
-    circles.append(plt.Circle((0, 0), (0.6), 
-                            color='blue', fill=True)) # .6
-    circles.append(plt.Circle((0, 0), (0.5), 
-                            color='black', fill=False)) # .5
-    circles.append(plt.Circle((0, 0), (0.4), 
-                            color='red', fill=True)) # .4
-    circles.append(plt.Circle((0, 0), (0.3), 
-                            color='black', fill=False)) # .3
-    circles.append(plt.Circle((0, 0), (0.2), 
-                            color='gold', fill=True)) # .2
-    circles.append(plt.Circle((0, 0), (0.1), 
-                            color='black', fill=False)) # .1
-        
-
-    for i in range(0, 10):
-        ax.add_patch(circles[i])
+    
+    makeFigure(ax)
 
     fig.savefig('plotcircles.png')
     
-    print(xCoords[1], yCoords[1])
 
-
-    plt.plot(xCoords, yCoords, ".")
+    plt.plot(xs, ys, ".")
     plt.plot(0, 0, color="#000000")
     plt.xlabel('X-axis')
     plt.ylabel('Y-axis')
@@ -159,3 +184,8 @@ if __name__ == "__main__":
     ax.set_aspect('equal', adjustable='box')
 
     plt.show()
+    return
+
+
+if __name__ == "__main__":
+    displayShots(500)
